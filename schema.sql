1ff58e9892717ef775e2f093d415963ff12ea838
Kazuhiro Osawa <yappo@shibuya.pl>
2014-09-28 14:04:18 +0900

    そもそもこっちに index 張らないと先にすすまない。。

--- schema.sql
+++ schema.sql
@@ -11,7 +11,10 @@ CREATE TABLE IF NOT EXISTS `login_log` (
   `user_id` int,
   `login` varchar(255) NOT NULL,
   `ip` varchar(255) NOT NULL,
-  `succeeded` tinyint NOT NULL
+  `succeeded` tinyint NOT NULL,
+
+  KEY (user_id),
+  KEY (ip)
 ) DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `last_login_success_user_id` (
